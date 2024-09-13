8ba92762cbf8bb5b39e9b7f18ed2067082ef12c8
brandon <brandonmaxwelltwo@gmail.com>
2024-09-13 09:44:54 +0300

    change type for sender and receiver table transaction

--- app/Jobs/ProcessTransactionJob.php
+++ app/Jobs/ProcessTransactionJob.php
@@ -33,7 +33,23 @@ class ProcessTransactionJob implements ShouldQueue
     }
 
     /**
-     * Execute the job.
+     * Handles the transaction processing by updating the status of the transaction,
+     * adjusting the sender's and receiver's account balances, and broadcasting
+     * the success or failure of the transaction.
+     *
+     * This method performs the following steps:
+     * - Starts a database transaction.
+     * - Updates the transaction status to "DONE".
+     * - Deducts the transaction amount from the sender's account and resets the frozen amount.
+     * - Adds the transaction amount to the receiver's account.
+     * - Commits the database transaction if successful.
+     * - If an exception occurs, rolls back the transaction and updates the transaction
+     *   status to "CANCELLED".
+     * - Broadcasts a success event on successful completion, or an error event if an
+     *   exception occurs.
+     *
+     * @return void
+     * @throws \Exception If any error occurs during the transaction process.
      */
     public function handle(): void
     {
